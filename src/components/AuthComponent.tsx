64a6b989ad2fd6cffb8d15839577e95660a6d3bd
2jyo7 <jyoti.kri2697@gmail.com>
2025-03-31 09:33:39 +0530

    changes in auth

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -7,7 +7,11 @@ import { useRouter } from "next/navigation";
 const AuthPage = () => {
   const { login, signup } = useAuth();
   const [isSignUp, setIsSignUp] = useState(false);
-  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
   const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,40 +29,54 @@ const AuthPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-bold mb-4">
-          {isSignUp ? "Sign Up" : "Login"}
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email"
-            className="w-full p-2 border rounded"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="Password"
-            className="w-full p-2 border rounded"
-            required
-          />
-          <Button label={isSignUp ? "Sign Up" : "Login"} />
-        </form>
-        <p
-          className="mt-4 text-blue-500 cursor-pointer text-sm"
-          onClick={() => setIsSignUp(!isSignUp)}
-        >
-          {isSignUp ? "Already have an account? Login" : "New user? Sign up"}
-        </p>
+    <section>
+      <div className="flex items-center justify-center min-h-screen bg-gradientPattern container">
+        <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg max-w-md w-full">
+          <h2 className="text-xl font-bold mb-4">
+            {isSignUp ? "Sign Up" : "Login"}
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {isSignUp && (
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Name"
+                className="w-full p-2 border rounded text-gray-700"
+                required
+              />
+            )}
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              className="w-full p-2 border rounded text-gray-700"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              className="w-full p-2 border rounded text-gray-700"
+              required
+            />
+
+            <Button label={isSignUp ? "Sign Up" : "Login"} />
+          </form>
+          <p
+            className="mt-4 text-white font-semibold cursor-pointer text-sm rounded-xl"
+            onClick={() => setIsSignUp(!isSignUp)}
+          >
+            {isSignUp ? "Already have an account? Login" : "New user? Sign up"}
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
